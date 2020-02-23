ac302bb7db019229fd0d06e5f9bafe36e95ce171
Neetshin <n33t5hin@gmail.com>
2020-02-23 12:32:51 +0900

    Improve URL regexps

--- src/regexps.spec.ts
+++ src/regexps.spec.ts
@@ -1,5 +1,19 @@
-import { MARKDOWN, URL } from "./regexps";
 import outdent from "outdent";
+import { MARKDOWN, URL } from "./regexps";
+
+const URL_EXAMPLE =
+  "https://discordapp.com" +
+  "/channels" +
+  "/443502244734828556" +
+  "/443678718792040448" +
+  "/678429687126556692";
+
+const PTB_URL_EXAMPLE =
+  "https://ptb.discordapp.com" +
+  "/channels" +
+  "/443502244734828556" +
+  "/443678718792040448" +
+  "/678429687126556692";
 
 describe("RegExps", () => {
   it("matches markdown blockquote", () => {
@@ -22,28 +36,33 @@ describe("RegExps", () => {
   });
 
   it("matches URL quote", () => {
-    const match = (
-      "https://discordapp.com" +
-      "/channels" +
-      "/443502244734828556" +
-      "/443678718792040448" +
-      "/678429687126556692"
-    ).match(URL);
+    const match = URL_EXAMPLE.match(URL);
     expect(match?.groups?.guildId).toBe("443502244734828556");
     expect(match?.groups?.channelId).toBe("443678718792040448");
     expect(match?.groups?.messageId).toBe("678429687126556692");
   });
 
   it("matches URL quote (Public Beta)", () => {
-    const match = (
-      "https://ptb.discordapp.com" +
-      "/channels" +
-      "/443502244734828556" +
-      "/443678718792040448" +
-      "/678429687126556692"
-    ).match(URL);
+    const match = PTB_URL_EXAMPLE.match(URL);
     expect(match?.groups?.guildId).toBe("443502244734828556");
     expect(match?.groups?.channelId).toBe("443678718792040448");
     expect(match?.groups?.messageId).toBe("678429687126556692");
   });
+
+  it("matches URL inside contents", () => {
+    const match = outdent`
+    foo bar
+    ${URL_EXAMPLE} foo bar
+    `.match(URL);
+    expect(match?.groups?.guildId).toBe("443502244734828556");
+    expect(match?.groups?.channelId).toBe("443678718792040448");
+    expect(match?.groups?.messageId).toBe("678429687126556692");
+  });
+
+  it('matches URL with trailing slash', () => {
+    const match = (URL_EXAMPLE + '/').match(URL);
+    expect(match?.groups?.guildId).toBe("443502244734828556");
+    expect(match?.groups?.channelId).toBe("443678718792040448");
+    expect(match?.groups?.messageId).toBe("678429687126556692");
+  })
 });
--- src/regexps.ts
+++ src/regexps.ts
@@ -1,2 +1,2 @@
 export const MARKDOWN = /^\>\s(?<text>.+)/m;
-export const URL = /https:\/\/(ptb\.)?discordapp.com\/channels\/(?<guildId>.+)\/(?<channelId>.+)\/(?<messageId>.+)/m;
+export const URL = /https:\/\/(ptb\.)?discordapp.com\/channels\/(?<guildId>[0-9]+)\/(?<channelId>[0-9]+)\/(?<messageId>[0-9]+)\/?/m;
