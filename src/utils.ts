9b32596728348a4d8826a889689dc6903e6f348b
Ryō Igarashi <n33t5hin@gmail.com>
2020-06-13 00:30:26 +0900

    Prettify

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import Discord from "discord.js";
+import Discord from 'discord.js';
 
 export const not = <T extends unknown[]>(func: (...args: T) => boolean) => (
   ...args: T
@@ -12,18 +12,19 @@ export const match = (regexp: RegExp) => (message: Discord.Message) =>
 export const fetchMessageByText = async (
   text: string,
   channel: Discord.Channel,
-  excludes: string[] = []
+  excludes: string[] = [],
 ) => {
-  if (!(channel instanceof Discord.TextChannel)) return;
+  if (!(channel instanceof Discord.TextChannel)) {
+    return;
+  }
 
   return channel.messages
     .fetch({ limit: 100 })
-    .then(collection => collection.array())
-    .then(messages =>
-      messages.find(
-        message =>
-          !excludes.includes(message.id) && message.content.includes(text)
-      )
+    .then((collection) => collection.array())
+    .then((messages) =>
+      messages.find((message) => {
+        return !excludes.includes(message.id) && message.content.includes(text);
+      }),
     );
 };
 
@@ -44,9 +45,9 @@ export const toEmbed = (message: Discord.Message) => {
     .setDescription(message.content)
     .setURL(message.url)
     .setTimestamp(message.createdTimestamp)
-    .setFooter("Quote");
+    .setFooter('Quote');
 
-  const avatar = message.author.avatarURL({ format: "png", size: 64 });
+  const avatar = message.author.avatarURL({ format: 'png', size: 64 });
   if (avatar) {
     embed.setAuthor(nickname, avatar);
   } else {
@@ -61,21 +62,19 @@ export const toEmbed = (message: Discord.Message) => {
 
 export const fetchWebhook = async (
   channel: Discord.TextChannel,
-  selfId: string
+  selfId: string,
 ) => {
-  const webhook = await channel
-    .fetchWebhooks()
-    .then(webhooks =>
-      webhooks.find(
-        ({ owner }) => owner instanceof Discord.User && owner.id === selfId
-      )
-    );
+  const webhook = await channel.fetchWebhooks().then((webhooks) =>
+    webhooks.find(({ owner }) => {
+      return owner instanceof Discord.User && owner.id === selfId;
+    }),
+  );
 
   if (webhook) return webhook;
-  return channel.createWebhook(process.env.DISCORD_WEBHOOK_NAME ?? "quote");
+  return channel.createWebhook(process.env.DISCORD_WEBHOOK_NAME ?? 'quote');
 };
 
-type WebhookSendParam = (Discord.WebhookMessageOptions & { split?: false });
+type WebhookSendParam = Discord.WebhookMessageOptions & { split?: false };
 
 export const mimic = async (
   content: string,
@@ -98,4 +97,4 @@ export const mimic = async (
 };
 
 export const removeEmptyLines = (text: string) =>
-  text.replace(/^\s*\n/gm, "").trim();
+  text.replace(/^\s*\n/gm, '').trim();
